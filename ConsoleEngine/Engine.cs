e192c3a77ad7f9f57ccd24413c382b2095927eaf
RealABlair <75645403+RealABlair@users.noreply.github.com>
2021-07-19 04:53:30 +0300

    Update Engine.cs

--- ConsoleEngine/Engine.cs
+++ ConsoleEngine/Engine.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -69,7 +69,7 @@ namespace ConsoleEngine
         public void GameLoop()
         {
             OnCreate();
-            while(enabled)
+            while (enabled)
             {
                 while (enabled)
                 {
@@ -102,7 +102,7 @@ namespace ConsoleEngine
 
         public void Clear(Pixel pixel)
         {
-            for(int i = 0; i < pixels.Length; i++)
+            for (int i = 0; i < pixels.Length; i++)
             {
                 pixels[i] = pixel;
             }
@@ -125,7 +125,7 @@ namespace ConsoleEngine
                     else
                     {
                         Pixel p = GetPixel(x, y);
-                        VisualOutput.ScreenBuilder.Append(Colorize(" ", p, ColorType.Background, default)); 
+                        VisualOutput.ScreenBuilder.Append(Colorize(" ", p, ColorType.Background, default));
                     }
                 }
                 VisualOutput.ScreenBuilder.AppendLine();
@@ -186,7 +186,7 @@ namespace ConsoleEngine
 
         public void FillRect(int x, int y, int width, int height, Pixel p)
         {
-            for(int X = 0; X < width; X++)
+            for (int X = 0; X < width; X++)
             {
                 for (int Y = 0; Y < height; Y++)
                 {
@@ -204,21 +204,157 @@ namespace ConsoleEngine
 
         public void FillTriangle(int x1, int y1, int x2, int y2, int x3, int y3, Pixel p)
         {
-            DrawLine(x1, y1, x2, y2, p);
-            DrawLine(x2, y2, x3, y3, p);
-            DrawLine(x3, y3, x1, y1, p);
-            for (int x = x1; x <= x2; x++)
+            void Swap(ref int a, ref int b) { int t = a; a = b; b = t; }
+            void MakeLine(int sx, int ex, int ny) { for (int i = sx; i <= ex; i++) Draw(i, ny, p); }
+
+            int t1x, t2x, y, minx, maxx, t1xp, t2xp;
+            bool changed1 = false;
+            bool changed2 = false;
+            int signx1, signx2, dx1, dy1, dx2, dy2;
+            int e1, e2;
+            // Sort vertices
+            if (y1 > y2) { Swap(ref y1, ref y2); Swap(ref x1, ref x2); }
+            if (y1 > y3) { Swap(ref y1, ref y3); Swap(ref x1, ref x3); }
+            if (y2 > y3) { Swap(ref y2, ref y3); Swap(ref x2, ref x3); }
+
+            t1x = t2x = x1; y = y1;   // Starting points
+            dx1 = x2 - x1; if (dx1 < 0) { dx1 = -dx1; signx1 = -1; }
+            else signx1 = 1;
+            dy1 = y2 - y1;
+
+            dx2 = x3 - x1; if (dx2 < 0) { dx2 = -dx2; signx2 = -1; }
+            else signx2 = 1;
+            dy2 = y3 - y1;
+
+            if (dy1 > dx1)
+            {   // swap values
+                Swap(ref dx1, ref dy1);
+                changed1 = true;
+            }
+            if (dy2 > dx2)
+            {   // swap values
+                Swap(ref dy2, ref dx2);
+                changed2 = true;
+            }
+
+            e2 = dx2 >> 1;
+            // Flat top, just process the second half
+            if (y1 == y2) goto next;
+            e1 = dx1 >> 1;
+
+            for (int i = 0; i < dx1;)
             {
-                for (int y = y1; y <= y2; y++)
+                t1xp = 0; t2xp = 0;
+                if (t1x < t2x) { minx = t1x; maxx = t2x; }
+                else { minx = t2x; maxx = t1x; }
+                // process first line until y value is about to change
+                while (i < dx1)
                 {
-                    DrawLine(x3, y3, x, y, p);
+                    i++;
+                    e1 += dy1;
+                    while (e1 >= dx1)
+                    {
+                        e1 -= dx1;
+                        if (changed1) t1xp = signx1;//t1x += signx1;
+                        else goto next1;
+                    }
+                    if (changed1) break;
+                    else t1x += signx1;
                 }
+            // Move line
+            next1:
+                // process second line until y value is about to change
+                while (true)
+                {
+                    e2 += dy2;
+                    while (e2 >= dx2)
+                    {
+                        e2 -= dx2;
+                        if (changed2) t2xp = signx2;//t2x += signx2;
+                        else goto next2;
+                    }
+                    if (changed2) break;
+                    else t2x += signx2;
+                }
+            next2:
+                if (minx > t1x) minx = t1x; if (minx > t2x) minx = t2x;
+                if (maxx < t1x) maxx = t1x; if (maxx < t2x) maxx = t2x;
+                MakeLine(minx, maxx, y);    // Draw line from min to max points found on the y
+                                            // Now increase y
+                if (!changed1) t1x += signx1;
+                t1x += t1xp;
+                if (!changed2) t2x += signx2;
+                t2x += t2xp;
+                y += 1;
+                if (y == y2) break;
+
+            }
+        next:
+            // Second half
+            dx1 = x3 - x2; if (dx1 < 0) { dx1 = -dx1; signx1 = -1; }
+            else signx1 = 1;
+            dy1 = y3 - y2;
+            t1x = x2;
+
+            if (dy1 > dx1)
+            {   // swap values
+                Swap(ref dy1, ref dx1);
+                changed1 = true;
+            }
+            else changed1 = false;
+
+            e1 = dx1 >> 1;
+
+            for (int i = 0; i <= dx1; i++)
+            {
+                t1xp = 0; t2xp = 0;
+                if (t1x < t2x) { minx = t1x; maxx = t2x; }
+                else { minx = t2x; maxx = t1x; }
+                // process first line until y value is about to change
+                while (i < dx1)
+                {
+                    e1 += dy1;
+                    while (e1 >= dx1)
+                    {
+                        e1 -= dx1;
+                        if (changed1) { t1xp = signx1; break; }//t1x += signx1;
+                        else goto next3;
+                    }
+                    if (changed1) break;
+                    else t1x += signx1;
+                    if (i < dx1) i++;
+                }
+            next3:
+                // process second line until y value is about to change
+                while (t2x != x3)
+                {
+                    e2 += dy2;
+                    while (e2 >= dx2)
+                    {
+                        e2 -= dx2;
+                        if (changed2) t2xp = signx2;
+                        else goto next4;
+                    }
+                    if (changed2) break;
+                    else t2x += signx2;
+                }
+            next4:
+
+                if (minx > t1x) minx = t1x; if (minx > t2x) minx = t2x;
+                if (maxx < t1x) maxx = t1x; if (maxx < t2x) maxx = t2x;
+                MakeLine(minx, maxx, y);
+                if (!changed1) t1x += signx1;
+                t1x += t1xp;
+                if (!changed2) t2x += signx2;
+                t2x += t2xp;
+                y += 1;
+                if (y > y3) return;
             }
         }
 
         public void DrawCurve(Point[] points, Pixel p)
         {
-            for(int i = 0; i < points.Length; i++)
+            for (int i = 0; i < points.Length; i++)
             {
                 Draw(points[i].x, points[i].y, p);
             }
