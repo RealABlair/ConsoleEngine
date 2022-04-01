3525d5dba1cef066914f4e9c2bd38976db515c3f
RealABlair <75645403+RealABlair@users.noreply.github.com>
2022-04-02 00:53:51 +0300

    Update Engine.cs

--- ConsoleEngine/Engine.cs
+++ ConsoleEngine/Engine.cs
@@ -39,6 +39,7 @@ namespace ConsoleEngine
             this.framerate = framerate;
             frameTimer = new Timer(1000.0f / framerate);
             pixels = new Pixel[Width * Height];
+            this.lastUpdateTime = DateTime.Now;
 
             for (int i = 0; i < pixels.Length; i++)
             {
