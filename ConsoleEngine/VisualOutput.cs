e2e6d3833bce9f91215037e663d91f22883a0253
RealABlair <75645403+RealABlair@users.noreply.github.com>
2022-06-29 21:57:55 +0300

    Update VisualOutput.cs

--- ConsoleEngine/VisualOutput.cs
+++ ConsoleEngine/VisualOutput.cs
@@ -1,15 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
+using System;
 using System.IO;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace ConsoleEngine
 {
     public static class VisualOutput
     {
         public static StringBuilder ScreenBuilder = new StringBuilder();
-        
+
+        static readonly BufferedStream stream;
+
+        static VisualOutput()
+        {
+            Console.OutputEncoding = Encoding.Unicode;
+
+            stream = new BufferedStream(Console.OpenStandardOutput(), 0x15000);
+        }
+
+        public static void WriteLine(string s) => Write(s + "\r\n");
+
+        public static void Write(string s)
+        {
+            byte[] b = new byte[s.Length << 1];
+            Encoding.Unicode.GetBytes(s, 0, s.Length, b, 0);
+
+            lock (stream)
+                stream.Write(b, 0, b.Length);
+        }
+
+        public static void Flush() { lock(stream) stream.Flush(); }
     }
 }
