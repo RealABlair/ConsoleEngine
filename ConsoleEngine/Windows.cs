47940f2adaade138b3b5a1dc694e9a74c400ebef
RealABlair <75645403+RealABlair@users.noreply.github.com>
2022-06-29 21:40:22 +0300

    Update Windows.cs

--- ConsoleEngine/Windows.cs
+++ ConsoleEngine/Windows.cs
@@ -16,6 +16,9 @@ namespace ConsoleEngine
         [DllImport(USER)]
         public static extern int SetWindowLong(IntPtr hWnd, int nIndex, uint dwNewLong);
 
+        [DllImport(USER)]
+        public static extern int GetWindowLong(IntPtr hWnd, int nIndex);
+
         [DllImport(KERNEL)]
         public static extern bool SetConsoleMode(IntPtr hConsoleHandle, uint dwMode);
 
@@ -37,7 +40,7 @@ namespace ConsoleEngine
         [DllImport(USER)]
         public static extern bool GetClientRect(IntPtr hWnd, out RECT lpRect);
 
-        [DllImport(USER, ExactSpelling = true, CharSet = CharSet.Auto)]
+        [DllImport(USER)]
         public static extern int GetSystemMetrics(int nIndex);
         [DllImport(KERNEL, SetLastError = true)]
         public static extern Int32 SetCurrentConsoleFontEx(IntPtr ConsoleOutput, bool MaximumWindow, ref CONSOLE_FONT_INFO_EX ConsoleCurrentFontEx);
@@ -54,11 +57,12 @@ namespace ConsoleEngine
         #region Consts
         public const string KERNEL = "kernel32.dll";
         public const string USER = "user32.dll";
+        public const string WINMM = "winmm.dll";
 
         public const int STD_INPUT_HANDLE = -10;
         public const int STD_OUTPUT_HANDLE = -11;
         public const int STD_ERROR_HANDLE = -12;
-
+            
 
         public const uint ENABLE_VIRTUAL_TERMINAL_PROCESSING = 0x0004;
         public const uint ENABLE_QUICK_EDIT = 0x0040;
@@ -109,6 +113,42 @@ namespace ConsoleEngine
 
             [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 32)] // Edit sizeconst if the font name is too big
             public string FaceName;
+        }
+        public enum GWL : int
+        {
+            GWL_WNDPROC = -4,
+            GWL_HINSTANCE = -6,
+            GWL_HWNDPARENT = -8,
+            GWL_STYLE = -16,
+            GWL_EXSTYLE = -20,
+            GWL_USERDATA = -21,
+            GWL_ID =  -12
+        }
+        [Flags]
+        public enum WS : uint
+        {
+            WS_BORDER = 0x800000,
+            WS_CAPTION = 0xc00000,
+            WS_CHILD = 0x40000000,
+            WS_CLIPCHILDREN = 0x2000000,
+            WS_CLIPSIBLINGS = 0x4000000,
+            WS_DISABLED = 0x8000000,
+            WS_DLGFRAME = 0x400000,
+            WS_GROUP = 0x20000,
+            WS_HSCROLL = 0x100000,
+            WS_MAXIMIZE = 0x1000000,
+            WS_MAXIMIZEBOX = 0x10000,
+            WS_MINIMIZE = 0x20000000,
+            WS_MINIMIZEBOX = 0x20000,
+            WS_OVERLAPPED = 0x0,
+            WS_OVERLAPPEDWINDOW = WS_OVERLAPPED | WS_CAPTION | WS_SYSMENU | WS_SIZEFRAME | WS_MINIMIZEBOX | WS_MAXIMIZEBOX,
+            WS_POPUP = 0x80000000u,
+            WS_POPUPWINDOW = WS_POPUP | WS_BORDER | WS_SYSMENU,
+            WS_SIZEFRAME = 0x40000,
+            WS_SYSMENU = 0x80000,
+            WS_TABSTOP = 0x10000,
+            WS_VISIBLE = 0x10000000,
+            WS_VSCROLL = 0x200000
         }
         #endregion
     }
