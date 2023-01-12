03e972521140fc50bdfe4198aa8c1797cb3592d6
Alex <75645403+RealABlair@users.noreply.github.com>
2023-01-12 15:16:06 +0300

    Update WorldRenderer.cs

--- Extensions/WorldRenderer.cs
+++ Extensions/WorldRenderer.cs
@@ -6,12 +6,18 @@ namespace FallingSand.Render
 {
     public class WorldRenderer
     {
-        private EngineChar engine;
         public Matrix3 worldMatrix { get; private set; }
 
-        public WorldRenderer(EngineChar engine)
+        public WorldRenderer(Action<int, int, char, ushort> DrawFunction)
         {
-            this.engine = engine;
+            this.ColoredDraw = DrawFunction;
+            worldMatrix = new Matrix3();
+            worldMatrix.Identity();
+        }
+
+        public WorldRenderer(Action<int, int, char> DrawFunction)
+        {
+            this.Draw = DrawFunction;
             worldMatrix = new Matrix3();
             worldMatrix.Identity();
         }
@@ -55,7 +61,7 @@ namespace FallingSand.Render
             return new Vector2(sx, sy);
         }
 
-        public void DrawChar(float x, float y, char c)
+        public void DrawChar(float x, float y, char c, ushort color = 0x000F)
         {
             Translate(x, y);
             Matrix3 matFinalInv = worldMatrix.Invert();
@@ -88,12 +94,19 @@ namespace FallingSand.Render
                     matFinalInv.Forward((float)ix, (float)iy, out nx, out ny);
                     if (ny < 0 || nx < 0 || nx >= 1f || ny >= 1f || (float.IsNaN(nx) || float.IsNaN(ny)))
                         continue;
-                    engine.Draw(ix, iy, c);
+                    if (Draw != null)
+                    {
+                        Draw.Invoke(ix, iy, c);
+                    }
+                    else if (ColoredDraw != null)
+                    {
+                        ColoredDraw.Invoke(ix, iy, c, color);
+                    }
                 }
             }
         }
 
-        public void DrawRect(float x, float y, float width, float height, char c)
+        public void DrawRect(float x, float y, float width, float height, char c, ushort color = 0x000F)
         {
             Translate(x, y);
             Matrix3 matFinalInv = worldMatrix.Invert();
@@ -126,12 +139,19 @@ namespace FallingSand.Render
                     matFinalInv.Forward((float)ix, (float)iy, out nx, out ny);
                     if (ny < 0 || nx < 0 || nx >= width || ny >= height || (float.IsNaN(nx) || float.IsNaN(ny)))
                         continue;
-                    engine.Draw(ix, iy, c);
+                    if (Draw != null)
+                    {
+                        Draw.Invoke(ix, iy, c);
+                    }
+                    else if (ColoredDraw != null)
+                    {
+                        ColoredDraw.Invoke(ix, iy, c, color);
+                    }
                 }
             }
         }
 
-        public void DrawSprite(float x, float y, CharSprite sprite)
+        public void DrawSprite(float x, float y, CharSprite sprite, ushort color = 0x000F)
         {
             Translate(x, y);
             Matrix3 matFinalInv = worldMatrix.Invert();
@@ -166,9 +186,21 @@ namespace FallingSand.Render
                         continue;
                     char p = sprite.GetPixel((int)(nx), (int)(ny));
                     if (p != ' ')
-                        engine.Draw(ix, iy, p);
+                    {
+                        if (Draw != null)
+                        {
+                            Draw.Invoke(ix, iy, p);
+                        }
+                        else if (ColoredDraw != null)
+                        {
+                            ColoredDraw.Invoke(ix, iy, p, color);
+                        }
+                    }
                 }
             }
         }
+
+        public Action<int, int, char, ushort> ColoredDraw { get; private set; }
+        public Action<int, int, char> Draw { get; private set; }
     }
 }
